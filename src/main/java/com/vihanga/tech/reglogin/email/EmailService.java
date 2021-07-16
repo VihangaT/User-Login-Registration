35f5a6f97c044cf5b11355850b4a7d4e2d908b07
vihanga <liyanage.vihanga99@gmail.com>
2021-07-17 01:14:41 +0530

    token confirmation via email implemented

--- src/main/java/com/vihanga/tech/reglogin/email/EmailService.java
+++ src/main/java/com/vihanga/tech/reglogin/email/EmailService.java
@@ -1,4 +1,44 @@
 package com.vihanga.tech.reglogin.email;
 
-public class EmailService {
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "UTF-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("hello@amigoscode.com");
+            mailSender.send(mimeMessage);
+
+        } catch (MessagingException ex) {
+            ex.getStackTrace();
+            LOGGER.error("failed to send the email", ex);
+            throw new IllegalStateException("failed to send Email");
+
+        }
+    }
 }
